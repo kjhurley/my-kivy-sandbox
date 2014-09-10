52792b50066219c243ea77251d151bb7759e7e0d
Kevin Hurley <kevin@hurleys.org.uk>
2014-09-10 16:11:04 +0100

    part 3 - started to integrate with jabber

--- __main__.py
+++ __main__.py
@@ -1,11 +1,32 @@
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.properties import ObjectProperty
+from sleekxmpp import ClientXMPP
+
+class JabberWrapper(object):
+    def __init__(self, jabber_id, password):
+        self._xmpp = ClientXMPP(jabber_id, password)
+
+    def connect(self):
+        self._xmpp.connect()
+        self._xmpp.process()
+        self._xmpp.send_presence()
+        self._xmpp.get_roster()
+        
+
 
 class AccountDetailsForm(AnchorLayout):
+    server_box = ObjectProperty()
+    username_box= ObjectProperty()
+    password_box = ObjectProperty()
     def login(self):
-        print "click it"
+        print self.server_box.text
+        print self.username_box.text
+        print self.password_box.text
 
 class Orkiv(App):
-    pass
-
+    def connect_to_jabber(self, jabber_id, password):
+        self.xmpp=JabberWrapper(jabber_id,password)
+        self.xmpp.connect()
+        
 Orkiv().run()
